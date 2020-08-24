d2bc11a27e1879077e884db7a9defecdcfd517a8
Lucas Conceiçao de Souza <lucaslssouza2015@gmail.com>
2020-08-24 20:08:13 -0300

    tirando traço das config do banco

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'admin',
 	'password' => 'lucas123',
 	'database' => 'intereventos',
-	'dbdriver' => 'mysq-li',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
