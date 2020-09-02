781afbb58291f66d83097692c6a025db4b65cf94
Lucas Conceiçao de Souza <lucaslssouza2015@gmail.com>
2020-09-01 21:33:25 -0300

    colocando get contents

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');  
 
-class Usuario extends CI_Controller {
+class Usuario extends MY_Controller {
 
 	public function __construct()
     {
@@ -19,7 +19,7 @@ class Usuario extends CI_Controller {
             case 'get':
                 $login = $_GET['login'];
                 $senha = $_GET['senha'];
-                echo json_encode($this->usuario_model->getUsers($login, $senha));
+                echo json_encode($this->usuario_model->getUsers($this->getContent()));
         }
     }
 }
--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -2,13 +2,14 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Usuario_model extends CI_Model{
-   public function getUsers($login="devBack", $senha="admin")
+   public function getUsers($dados)
    {
-       return $this->db->select("*")
-            ->where("usu_email", $login)
-            ->or_where("usu_usuario", $login)
-            ->where("usu_senha", md5($senha))
+      if ($dados) {
+         return $this->db->select("*")
+            ->where("usu_email", $dados['login'])
+            ->or_where("usu_usuario", $dados['login'])
+            ->where("usu_senha", md5($dados['senha']))
             ->get("usuario")->result_array();
-      //  return "teste end point";
+      }
    }
 }
