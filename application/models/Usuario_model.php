31650c3ae94945f9ddbcdae0772d7cef14ec3b5e
Lucas Conceiçao de Souza <lucaslssouza2015@gmail.com>
2020-10-01 19:27:54 -0300

    arrumando parametro login

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -14,8 +14,7 @@ class Usuario_model extends CI_Model{
          ->where("email_usuario", $dados['login'])
          ->or_where("nick_usuario", $dados['login'])
          ->having("senha_usuario", md5($dados['senha']))
-         ->having("tipo_usuario", $dados['tipo'])
-            ->get("usuario")->result_array();
+         ->get("usuario")->result_array();
       }else{
          return false;
       }
@@ -24,11 +23,11 @@ class Usuario_model extends CI_Model{
    public function cadastraUsuario($dados){
       if (!empty($dados)) {
          if (!empty($this->validaNickUsuario($dados['email_usuario'], "email_usuario"))) {
-            return "ja existe um email com esse nome " . $dados['email_usuario'];
+            return "ja existe este email cadastrado -> " . $dados['email_usuario'];
          }
       
          if (!empty($this->validaNickUsuario($dados["nick_usuario"], "nick_usuario"))) {
-            return "ja existe um nick com esse nome " . $dados['nick_usuario'];
+            return "ja existe este username -> " . $dados['nick_usuario'];
          }
 
          $this->db->set("email_usuario", $dados['email_usuario']);
